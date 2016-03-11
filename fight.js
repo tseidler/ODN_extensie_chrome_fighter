6aca0cf45461197673e86c3e8e90c325a2675661
tseidler <terry@salteh.net>
2016-03-11 14:56:37 +0100

    Spawn fireball & ken

--- fight.js
+++ fight.js
@@ -13,8 +13,33 @@ function catchClck(event) {
   event.preventDefault();
   event.stopPropagation();
 
+
   var fightTarget = event.target.closest('div');
-  fightTarget.remove();
+  var ken = document.getElementById('ODN_FIGHTER');
+  var xPos = Math.max(event.pageX - 500, 10);
+  var yPos = Math.max(event.pageY - 50, 0);
+
+  ken.style.top =  yPos + 'px';
+  ken.style.left = xPos + 'px';
+  ken.style.display = 'block';
+  ken.setAttribute('class', 'attack');
+
+  setTimeout(function hideKen () {
+    ken.style.display = 'none';
+    ken.setAttribute('class', '');
+  }, 2000);
+  setTimeout(function () {
+    spawnFireball(xPos + 40, yPos, ken, fightTarget);
+  }, 250);
+}
+
+function spawnFireball(x, y, attachTo, target) {
+  var ball = document.createElement('div');
+  ball.setAttribute('class', 'fireball');
+  ball.style.backgroundImage = 'url(' + chrome.extension.getURL('images/sprites.png') + ')';
+  ball.style.left = x + 'px';
+  ball.style.top = y + 'px';
+  document.body.appendChild(ball);
 }
 
 function addDiv() {
