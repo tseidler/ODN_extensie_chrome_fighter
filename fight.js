b0dbe378ded231a631a15dd2f48cec1aec048c08
tseidler <terry@salteh.net>
2016-03-11 13:55:30 +0100

    Add ODN_fighter div

--- fight.js
+++ fight.js
@@ -1,10 +1,11 @@
 chrome.runtime.onMessage.addListener(processMessage);
+addDiv();
 
 function processMessage(request, sender, sendResponse) {
   if(request.fighting === true) {
-    document.addEventListener('click', catchClck);
+    document.body.addEventListener('click', catchClck);
   } else {
-    document.removeEventListener('click', catchClck);
+    document.body.removeEventListener('click', catchClck);
   }
 }
 
@@ -12,7 +13,13 @@ function catchClck(event) {
   event.preventDefault();
   event.stopPropagation();
 
-
   var fightTarget = event.target.closest('div');
   fightTarget.remove();
+}
+
+function addDiv() {
+  var fightDiv = document.createElement('div');
+  fightDiv.setAttribute('id', 'ODN_FIGHTER');
+  fightDiv.style.backgroundImage = 'url(' + chrome.extension.getURL('images/sprites.png') + ')';
+  document.body.appendChild(fightDiv);
 }
