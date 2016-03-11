f71713aa50c6dbfcd771d3774eeccaf18cb0e9d8
tseidler <terry@salteh.net>
2016-03-11 15:19:59 +0100

    Allow multiple kens

--- fight.js
+++ fight.js
@@ -1,6 +1,4 @@
 chrome.runtime.onMessage.addListener(processMessage);
-addDiv();
-
 function processMessage(request, sender, sendResponse) {
   if(request.fighting === true) {
     document.body.addEventListener('click', catchClck);
@@ -13,38 +11,44 @@ function catchClck(event) {
   event.preventDefault();
   event.stopPropagation();
 
-
   var fightTarget = event.target.closest('div');
-  var ken = document.getElementById('ODN_FIGHTER');
   var xPos = Math.max(event.pageX - 500, 10);
   var yPos = Math.max(event.pageY - 50, 0);
 
+  var ken = document.createElement('div');
+  ken.setAttribute('class', 'sprite attack');
+  ken.style.backgroundImage = 'url(' + chrome.extension.getURL('images/sprites.png') + ')';
   ken.style.top =  yPos + 'px';
   ken.style.left = xPos + 'px';
-  ken.style.display = 'block';
-  ken.setAttribute('class', 'attack');
+  document.body.appendChild(ken);
 
-  setTimeout(function hideKen () {
-    ken.style.display = 'none';
-    ken.setAttribute('class', '');
-  }, 2000);
   setTimeout(function () {
     spawnFireball(xPos + 40, yPos, ken, fightTarget);
   }, 250);
 }
 
-function spawnFireball(x, y, attachTo, target) {
+function spawnFireball(x, y, source, target) {
   var ball = document.createElement('div');
-  ball.setAttribute('class', 'fireball');
+  var ballX = x;
+  var targetBox = target.getBoundingClientRect();
+
+  var targetLeft = targetBox.left;
+
+  ball.setAttribute('class', 'sprite fireball');
   ball.style.backgroundImage = 'url(' + chrome.extension.getURL('images/sprites.png') + ')';
-  ball.style.left = x + 'px';
+  ball.style.left = ballX + 'px';
   ball.style.top = y + 'px';
   document.body.appendChild(ball);
-}
 
-function addDiv() {
-  var fightDiv = document.createElement('div');
-  fightDiv.setAttribute('id', 'ODN_FIGHTER');
-  fightDiv.style.backgroundImage = 'url(' + chrome.extension.getURL('images/sprites.png') + ')';
-  document.body.appendChild(fightDiv);
+  var interval = setInterval(function moveBall() {
+    ballX+=5;
+    ball.style.left = ballX + 'px';
+
+    if(ballX > targetLeft || ballX > screen.width) {
+      target.remove();
+      ball.remove();
+      source.remove();
+      clearInterval(interval);
+    }
+  }, 25);
 }
